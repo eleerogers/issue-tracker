2ccdd7ba04c75df93f7eff4eb8a1a3522305e8f8
Lee Rogers <edwinleerogers@gmail.com>
2023-12-15 23:23:08 -0800

    Improve loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,21 +1,19 @@
 "use client"
 
-import { Button, TextField, Callout, Text } from '@radix-ui/themes'
-import dynamic from 'next/dynamic'
-import { useForm, Controller } from 'react-hook-form'
-import 'easymde/dist/easymde.min.css'
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { issueSchema } from '@/app/validationSchemas'
-import { z } from 'zod'
 import ErrorMessage from '@/app/components/ErrorMessage'
 import Spinner from '@/app/components/Spinner'
+import { issueSchema } from '@/app/validationSchemas'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Issue } from '@prisma/client'
-
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
+import { Button, Callout, TextField } from '@radix-ui/themes'
+import axios from 'axios'
+import 'easymde/dist/easymde.min.css'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import SimpleMDE from 'react-simplemde-editor'
+import { z } from 'zod'
 
 type IssueFormData = z.infer<typeof issueSchema>
 
@@ -40,6 +38,7 @@ const IssueForm = ({ issue }: Props) => {
         await axios.post('/api/issues', data);
       }
       router.push('/issues')
+      router.refresh()
     } catch (error) {
       setError('Something went wrong...')
       setIsSubmitting(false)
