c1a9c925ac6fcd90766e08f7bc21f621a46f3db4
Lee Rogers <edwinleerogers@gmail.com>
2023-12-18 20:45:55 -0800

    restructure code to avoid conflicting loading files / duplicate skeletons

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -6,7 +6,7 @@ export default function IssueActions() {
   return (
     <div className='mb-5'>
         <Button>
-          <Link href='issues/new'>New Issue</Link>
+          <Link href='/issues/new'>New Issue</Link>
         </Button>
       </div>
   )
