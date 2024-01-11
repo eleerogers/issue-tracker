703a13f99ad57c05900d59d6a1cb9157ea853aac
Lee Rogers <edwinleerogers@gmail.com>
2024-01-10 22:35:24 -0800

    reformat IssueDetails

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -1,22 +1,21 @@
-import { IssueStatusBadge } from '@/app/components'
-import { Issue } from '@prisma/client'
-import { Heading, Flex, Card, Text } from '@radix-ui/themes'
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
 
 const IssueDetails = ({ issue }: { issue: Issue }) => {
   return (
     <>
       <Heading>{issue.title}</Heading>
-      <Flex gap="5" my='2'>
+      <Flex gap="5" my="2">
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card className='prose max-w-full' mt='4'>
+      <Card className="prose max-w-full" mt="4">
         <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </>
-  )
-}
+  );
+};
 
-export default IssueDetails
+export default IssueDetails;
